d8d802c259bc13c783c8d57a08dc6e68f332c8f3
beatriz kenup <beatriz.kenup@indicium.tech>
2023-03-14 17:59:16 -0300

    fix dbt_utils_day

--- models/staging/dbt_utils_day.sql
+++ models/staging/dbt_utils_day.sql
@@ -3,15 +3,17 @@
 {% endset %}
 
 {% if execute %}
-{% set today = run_query(my_query).columns[0].values()[0] %}
-{% set tomorrow = dbt_utils.dateadd('day', 1, today) %}
-{% else %}
-{% set tomorrow = ' ' %}
+    {% set today = run_query(my_query).columns[0].values()[0] %}
+    {% set tomorrow = dbt_utils.dateadd('day', 1, today) %}
+    {% set start_date = var('dbt_airflow_monitoring')['airflow_monitoring_start_date'] %}
+    {% else %}
+    {% set tomorrow = ' ' %}
+    {% set start_date = ' ' %}
 {% endif %}
 
 {{ dbt_utils.date_spine(
-datepart="day",
-start_date="'2016-01-01'",
-end_date="cast('{{tomorrow}}' as date)"
+    datepart="day",
+    start_date=start_date,
+    end_date="cast('{{tomorrow}}' as date)"
 )
 }}
