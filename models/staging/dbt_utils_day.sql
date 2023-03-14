3e0429d50ac886cfeff3a6a13f1de6cde497b10f
beatriz kenup <beatriz.kenup@indicium.tech>
2023-03-14 14:30:29 -0300

    fix dbt_utils_day

--- models/staging/dbt_utils_day.sql
+++ models/staging/dbt_utils_day.sql
@@ -1,6 +1,17 @@
+{% set my_query %}
+    select cast({{dbt_utils.current_timestamp()}} as date)
+{% endset %}
+
+{% if execute %}
+{% set today = run_query(my_query).columns[0].values()[0] %}
+{% set tomorrow = dbt_utils.dateadd('day', 1, today) %}
+{% else %}
+{% set tomorrow = ' ' %}
+{% endif %}
+
 {{ dbt_utils.date_spine(
-    datepart="day",
-    start_date="'2016-01-01'",
-    end_date="dateadd(month, 1, date_trunc('month', getdate()))"
-   )
+datepart="day",
+start_date="'2016-01-01'",
+end_date="cast('{{tomorrow}}' as date)"
+)
 }}
