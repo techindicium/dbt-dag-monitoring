38cb34edb0279abe3ef6aa7c6d930f924af5fc54
Vitor Avancini <vitor.avancini@indicium.tech>
2023-12-04 11:19:34 -0300

    [wip] adding databricks models

---
+++ models/staging/databricks_workflow_sources/stg_airflow_monitoring_raw_airflow_monitoring_dag_run.sql
@@ -0,0 +1,17 @@
+with
+    renamed as (
+        select
+        number_in_job as dag_run_id
+        , job_id as dag_id
+        , from_unixtime(start_time / 1000)::date run_date
+        , state.result_state as dag_state
+        , `trigger`as external_trigger
+        , from_unixtime(start_time / 1000) as execution_start_date
+        , from_unixtime(end_time / 1000) execution_end_date
+        , execution_duration / 1000 as duration
+        , run_type
+        , run_id
+        from dev_vitor_avancini.databricks_job_runs
+ )
+select *
+from renamed
---
+++ models/staging/databricks_workflow_sources/stg_airflow_monitoring_raw_airflow_monitoring_task_fail.sql
@@ -0,0 +1,18 @@
+with
+    renamed as (
+        select distinct
+            activityRunId as task_fail_id
+            , activityRunId as task_id
+            , pipelineName as dag_id
+            , pipelineRunId as run_id
+            , {{ cast_as_date('activityRunStart') }} as execution_date
+            , activityRunStart as execution_start_date
+            , activityRunEnd as execution_end_date
+            , durationInMs / 1000 as duration
+            , "not_implemented_for_adf" as map_index
+        from {{ source('raw_adf_monitoring', 'adf_activity_runs') }}
+        where status in ('TimedOut', 'Cancelled', 'Failed')
+    )
+select *
+from renamed
+
