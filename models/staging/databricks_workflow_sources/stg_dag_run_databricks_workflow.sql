3768be25120ff590bedf1d4a91822bc63d1d0af5
Suelen <ext.suelen.rodrigues@zamp.com.br>
2024-02-08 08:45:01 -0300

    Adjustments on stgs

--- models/staging/databricks_workflow_sources/stg_dag_run_databricks_workflow.sql
+++ models/staging/databricks_workflow_sources/stg_dag_run_databricks_workflow.sql
@@ -2,15 +2,15 @@ with renamed as (
     select
         {{ cast_as_string('run_id') }} as dag_run_id
         , {{ cast_as_string('job_id') }} as dag_id
-        , from_unixtime(start_time / 1000) as run_date
-        , state.result_state as dag_state
+        , to_timestamp(start_time / 1000) as run_date
+        , state:result_state as dag_state
         , `trigger` as external_trigger
-        , from_unixtime(start_time / 1000) as execution_start_date
-        , from_unixtime(end_time / 1000) as execution_end_date
+        , to_timestamp(start_time / 1000) as execution_start_date
+        , to_timestamp(end_time / 1000) as execution_end_date
         , execution_duration / 1000 as duration
         , run_type
         , {{ cast_as_string('run_id') }} as run_id
     from
-        {{ source('raw_databricks_workflow_monitoring', 'databricks_job_runs') }}
+        {{ source('raw_databricks_workflow_monitoring', 'job_runs') }}
 )
 select * from renamed
