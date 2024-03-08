ef28dd35da67d50dc3c5c982201d816d79c993d2
suelen-nfr <suelen.rodrigues@indicium.tech>
2024-03-08 13:59:13 -0300

    standard adjustments

--- models/staging/databricks_workflow_sources/stg_task_instance_databricks_workflow.sql
+++ models/staging/databricks_workflow_sources/stg_task_instance_databricks_workflow.sql
@@ -8,12 +8,12 @@ with tasks as (
 renamed as (
     select 
         cast(tasks.key as string) as task_id
-        , cast(job_id as string) as dag_id
-        , cast(run_id as string) as run_id
+        , cast(tasks.job_id as string) as dag_id
+        , cast(tasks.run_id as string) as run_id
         , to_timestamp( tasks.start_time / 1000) as execution_date
         , to_timestamp( tasks.start_time / 1000) as execution_start_date
         , to_timestamp( tasks.end_time / 1000) as execution_end_date
-        , (execution_duration / 1000) as duration
+        , (tasks.execution_duration / 1000) as duration
         , tasks.state:result_state as state_task_instance
         , tasks.attempt_number as try_number
         , tasks:notebook_task:notebook_path as hostname
