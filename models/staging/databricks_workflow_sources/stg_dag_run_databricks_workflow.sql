a8fff078978ac3077071eec192f3d95f3aea3b0e
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-08-14 11:24:06 -0300

    changes 2

--- models/staging/databricks_workflow_sources/stg_dag_run_databricks_workflow.sql
+++ models/staging/databricks_workflow_sources/stg_dag_run_databricks_workflow.sql
@@ -3,7 +3,7 @@ with renamed as (
         {{ cast_as_string('run_id') }} as dag_run_id
         , {{ cast_as_string('job_id') }} as dag_id
         , to_timestamp(start_time / 1000) as run_date
-        , state:result_state as dag_state
+        , state.result_state as dag_state
         , `trigger` as external_trigger
         , to_timestamp(start_time / 1000) as execution_start_date
         , to_timestamp(end_time / 1000) as execution_end_date
