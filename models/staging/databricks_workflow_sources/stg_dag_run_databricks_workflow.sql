d1283693c63ae05f3518d4610f6a01cd8f5a3fd2
Carlos Magno <carlos.magno@indicium.tech>
2024-08-26 17:35:16 -0300

    Feature: Adjust models to run in Databricks dealing with struct data type

--- models/staging/databricks_workflow_sources/stg_dag_run_databricks_workflow.sql
+++ models/staging/databricks_workflow_sources/stg_dag_run_databricks_workflow.sql
@@ -1,16 +1,17 @@
-with renamed as (
-    select
-        {{ cast_as_string('run_id') }} as dag_run_id
-        , {{ cast_as_string('job_id') }} as dag_id
-        , to_timestamp(start_time / 1000) as run_date
-        , state:result_state as dag_state
-        , `trigger` as external_trigger
-        , to_timestamp(start_time / 1000) as execution_start_date
-        , to_timestamp(end_time / 1000) as execution_end_date
-        , execution_duration / 1000 as duration
-        , run_type
-        , {{ cast_as_string('run_id') }} as run_id
-    from
-        {{ source('raw_databricks_workflow_monitoring', 'job_runs') }}
+with
+    renamed as (
+        select
+            {{ cast_as_string('run_id') }} as dag_run_id
+            , {{ cast_as_string('job_id') }} as dag_id
+            , to_timestamp(start_time / 1000) as run_date
+            , state.result_state as dag_state
+            , `trigger` as external_trigger
+            , to_timestamp(start_time / 1000) as execution_start_date
+            , to_timestamp(end_time / 1000) as execution_end_date
+            , execution_duration / 1000 as duration
+            , run_type
+            , {{ cast_as_string('run_id') }} as run_id
+        from {{ source('raw_databricks_workflow_monitoring', 'job_runs') }}
 )
-select * from renamed
+select *
+from renamed
