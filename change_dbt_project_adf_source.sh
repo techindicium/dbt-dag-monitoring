9686d4fa43bd10c1f651a845d285d01c4193c34e
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-08-27 15:02:39 -0300

    sh working

--- change_dbt_project_adf_source.sh
+++ change_dbt_project_adf_source.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # Enable adf sources in dbt_project.yml
-sed -i 's/raw_adf_monitoring:\n\(\s\+\)+enabled: false/raw_adf_monitoring:\n\1+enabled: true/' dbt_project.yml
-
+sed -i '/raw_adf_monitoring:/,/enabled:/s/enabled: false/enabled: true/' dbt_project.yml
 # Disable databricks sources in dbt_project.yml
-sed -i 's/raw_databricks_workflow_monitoring:\n\(\s\+\)+enabled: true/raw_databricks_workflow_monitoring:\n\1+enabled: false/' dbt_project.yml
+sed -i 's/\(raw_databricks_workflow_monitoring:\s*\n\s*+enabled:\s*\)true/\1false/' dbt_project.yml
 
 # Enable adf models in dbt_project.yml
-sed -i 's/adf_sources:\n\(\s\+\)+enabled: false/adf_sources:\n\1+enabled: true/' dbt_project.yml
+sed -i '/adf_sources:/,/enabled:/s/enabled: false/enabled: true/' dbt_project.yml
 
 # Disable databricks_workflow models in dbt_project.yml
-sed -i 's/databricks_workflow_sources:\n\(\s\+\)+enabled: true/databricks_workflow_sources:\n\1+enabled: false/' dbt_project.yml
+sed -i '/databricks_workflow_sources:/,/enabled:/s/enabled: true/enabled: false/' dbt_project.yml
 
 # Update the enabled_sources in dbt_project.yml
-sed -i 's/enabled_sources: \[.*\]/enabled_sources: \[adf\]/' dbt_project.yml
+sed -i "s/enabled_sources: \[.*\]/enabled_sources: \['adf'\]/" dbt_project.yml
