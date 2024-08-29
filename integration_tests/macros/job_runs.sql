441d60715a90021290ec3af33fc92579f20c4cc2
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-08-29 15:22:49 -0300

    macros for databricks and snowflake

--- integration_tests/macros/job_runs.sql
+++ integration_tests/macros/job_runs.sql
@@ -1,4 +1,8 @@
-{%- macro job_runs() -%}
+{% macro job_runs() -%}
+    {{ return(adapter.dispatch('job_runs')()) }}
+{%- endmacro %}
+
+{%- macro databricks__job_runs() -%}
 {% set create_table %}
 create or replace table `{{ target.database }}`.{{ target.schema }}.job_runs(
     cleanup_duration BIGINT,
@@ -334,6 +338,389 @@ INSERT INTO `{{ target.database }}`.{{ target.schema }}.job_runs VALUES
     'ONE_TIME',
     '2024-08-11T07:12:47.784Z'
 );
+{% endset %}
+
+{% do run_query(create_table) %}
+{% do log("finished creating table job_runs", info=true) %}
+
+{% do run_query(insert_table) %}
+{% do log("finished insert table job_runs", info=true) %}
+
+{%- endmacro -%}
+
+{%- macro snowflake__job_runs() -%}
+{% set create_table %}
+CREATE OR REPLACE TABLE "{{ target.database }}.{{ target.schema }}.job_runs" (
+    cleanup_duration BIGINT,
+    creator_user_name STRING,
+    end_time BIGINT,
+    execution_duration BIGINT,
+    job_clusters ARRAY (
+        OBJECT(
+            'job_cluster_key', STRING,
+            'new_cluster', OBJECT(
+                'cluster_name', STRING,
+                'data_security_mode', STRING,
+                'driver_node_type_id', STRING,
+                'enable_elastic_disk', BOOLEAN,
+                'node_type_id', STRING,
+                'num_workers', BIGINT,
+                'runtime_engine', STRING,
+                'spark_version', STRING
+            )
+        )
+    ),
+    job_id BIGINT,
+    number_in_job BIGINT,
+    original_attempt_run_id BIGINT,
+    run_duration BIGINT,
+    run_id BIGINT,
+    run_name STRING,
+    run_page_url STRING,
+    run_type STRING,
+    schedule OBJECT(
+        'pause_status', STRING,
+        'quartz_cron_expression', STRING,
+        'timezone_id', STRING
+    ),
+    setup_duration BIGINT,
+    start_time BIGINT,
+    state OBJECT(
+        'life_cycle_state', STRING,
+        'result_state', STRING,
+        'state_message', STRING,
+        'user_cancelled_or_timedout', BOOLEAN
+    ),
+    tasks ARRAY(
+        OBJECT(
+            'attempt_number', BIGINT,
+            'cleanup_duration', BIGINT,
+            'cluster_instance', OBJECT(
+                'cluster_id', STRING,
+                'spark_context_id', STRING
+            ),
+            'dbt_task', OBJECT(
+                'commands', ARRAY(STRING),
+                'project_directory', STRING,
+                'schema', STRING,
+                'warehouse_id', STRING
+            ),
+            'depends_on', ARRAY(
+                OBJECT(
+                    'task_key', STRING
+                )
+            ),
+            'end_time', BIGINT,
+            'execution_duration', BIGINT,
+            'existing_cluster_id', STRING,
+            'git_source', OBJECT(
+                'git_branch', STRING,
+                'git_provider', STRING,
+                'git_snapshot', OBJECT(
+                    'used_commit', STRING
+                ),
+                'git_tag', STRING,
+                'git_url', STRING
+            ),
+            'libraries', ARRAY(
+                OBJECT(
+                    'pypi', OBJECT(
+                        'package', STRING
+                    )
+                )
+            ),
+            'notebook_task', OBJECT(
+                'notebook_path', STRING,
+                'source', STRING
+            ),
+            'run_id', BIGINT,
+            'setup_duration', BIGINT,
+            'start_time', BIGINT,
+            'state', OBJECT(
+                'life_cycle_state', STRING,
+                'result_state', STRING,
+                'state_message', STRING,
+                'user_cancelled_or_timedout', BOOLEAN
+            ),
+            'task_key', STRING
+        )
+    ),
+    trigger STRING,
+    insertedDate TIMESTAMP
+);
+
+{% endset %}
+
+{% set insert_table %}
+
+INSERT INTO `{{ target.database }}`.{{ target.schema }}.job_runs VALUES
+(
+    0,
+    'marcus.canto@indicium.tech',
+    1720785705439,
+    0,
+    ARRAY_CONSTRUCT(
+        OBJECT_CONSTRUCT(
+            'job_cluster_key', NULL,
+            'new_cluster', OBJECT_CONSTRUCT(
+                'cluster_name', NULL,
+                'data_security_mode', NULL,
+                'driver_node_type_id', NULL,
+                'enable_elastic_disk', NULL,
+                'node_type_id', NULL,
+                'num_workers', NULL,
+                'runtime_engine', NULL,
+                'spark_version', NULL
+            )
+        )
+    ),
+    724712624070673,
+    539935187772655,
+    539935187772655,
+    524911,
+    539935187772655,
+    '[dev marcus_canto] dag_monitoring_notebook_job',
+    'https://adb-124029731635903.3.azuredatabricks.net/?o=124029731635903#job/724712624070673/run/539935187772655',
+    'JOB_RUN',
+    OBJECT_CONSTRUCT(
+        'pause_status', NULL,
+        'quartz_cron_expression', NULL,
+        'timezone_id', NULL
+    ),
+    0,
+    1720785180528,
+    OBJECT_CONSTRUCT(
+        'life_cycle_state', 'TERMINATED',
+        'result_state', 'SUCCESS',
+        'state_message', NULL,
+        'user_cancelled_or_timedout', FALSE
+    ),
+    ARRAY_CONSTRUCT(
+        OBJECT_CONSTRUCT(
+            'attempt_number', 0,
+            'cleanup_duration', 0,
+            'cluster_instance', OBJECT_CONSTRUCT(
+                'cluster_id', '0726-190025-l9rjt3er',
+                'spark_context_id', NULL
+            ),
+            'dbt_task', NULL,
+            'depends_on', ARRAY_CONSTRUCT(
+                OBJECT_CONSTRUCT('task_key', 'meltano_databricks_ops')
+            ),
+            'end_time', 172115372793563,
+            'execution_duration', 0,
+            'existing_cluster_id', '0726-190025-l9rjt3er',
+            'git_source', OBJECT_CONSTRUCT(
+                'git_branch', 'git_task',
+                'git_provider', 'bitbucketCloud',
+                'git_snapshot', NULL,
+                'git_tag', NULL,
+                'git_url', 'https://bitbucket.org/indiciumtech/platform_meltano_on_databricks'
+            ),
+            'libraries', NULL,
+            'notebook_task', OBJECT_CONSTRUCT(
+                'notebook_path', 'src/execute_table_dbr',
+                'source', 'GIT'
+            ),
+            'run_id', 703673872655804,
+            'setup_duration', 0,
+            'start_time', 172115150810239,
+            'state', OBJECT_CONSTRUCT(
+                'life_cycle_state', 'BLOCKED',
+                'result_state', NULL,
+                'state_message', '',
+                'user_cancelled_or_timedout', FALSE
+            ),
+            'task_key', 'execute_table_dbr'
+        ),
+        OBJECT_CONSTRUCT(
+            'attempt_number', 0,
+            'cleanup_duration', 0,
+            'cluster_instance', OBJECT_CONSTRUCT(
+                'cluster_id', '0726-190025-l9rjt3er',
+                'spark_context_id', '2280680787151510917'
+            ),
+            'dbt_task', NULL,
+            'depends_on', ARRAY_CONSTRUCT(
+                OBJECT_CONSTRUCT('task_key', 'meltano_init')
+            ),
+            'end_time', 172078572305439,
+            'execution_duration', 0,
+            'existing_cluster_id', '0726-190025-l9rjt3er',
+            'git_source', OBJECT_CONSTRUCT(
+                'git_branch', 'git_task',
+                'git_provider', 'bitbucketCloud',
+                'git_snapshot', OBJECT_CONSTRUCT(
+                    'used_commit', '2fc709b03d0059a18a9b994504e270c9114c1b07'
+                ),
+                'git_tag', NULL,
+                'git_url', 'https://bitbucket.org/indiciumtech/platform_meltano_on_databricks'
+            ),
+            'libraries', ARRAY_CONSTRUCT(
+                OBJECT_CONSTRUCT('pypi', OBJECT_CONSTRUCT('package', 'meltano==3.3.2'))
+            ),
+            'notebook_task', OBJECT_CONSTRUCT(
+                'notebook_path', 'src/meltano_databricks_ops',
+                'source', 'GIT'
+            ),
+            'run_id', 79128842213838,
+            'setup_duration', 1000,
+            'start_time', 172115372792333,
+            'state', OBJECT_CONSTRUCT(
+                'life_cycle_state', 'RUNNING',
+                'result_state', NULL,
+                'state_message', 'In run',
+                'user_cancelled_or_timedout', FALSE
+            ),
+            'task_key', 'meltano_databricks_ops'
+        )
+    ),
+    'ONE_TIME',
+    '2024-08-11T07:12:47.784Z'
+),
+(
+    0,
+    'marcus.canto@indicium.tech',
+    1720781204456,
+    0,
+    ARRAY_CONSTRUCT(
+        OBJECT_CONSTRUCT(
+            'job_cluster_key', NULL,
+            'new_cluster', OBJECT_CONSTRUCT(
+                'cluster_name', NULL,
+                'data_security_mode', NULL,
+                'driver_node_type_id', NULL,
+                'enable_elastic_disk', NULL,
+                'node_type_id', NULL,
+                'num_workers', NULL,
+                'runtime_engine', NULL,
+                'spark_version', NULL
+            )
+        )
+    ),
+    388879741463141,
+    1104206444717471,
+    1104206444717471,
+    114044,
+    1104206444717471,
+    '[dev marcus_canto] dag_monitoring_notebook_job',
+    'https://adb-124029731635903.3.azuredatabricks.net/?o=124029731635903#job/388879741463141/run/1104206444717471',
+    'JOB_RUN',
+    OBJECT_CONSTRUCT(
+        'pause_status', NULL,
+        'quartz_cron_expression', NULL,
+        'timezone_id', NULL
+    ),
+    0,
+    1720781090412,
+    OBJECT_CONSTRUCT(
+        'life_cycle_state', 'TERMINATED',
+        'result_state', 'SUCCESS',
+        'state_message', NULL,
+        'user_cancelled_or_timedout', FALSE
+    ),
+    ARRAY_CONSTRUCT(
+        OBJECT_CONSTRUCT(
+            'attempt_number', 0,
+            'cleanup_duration', 0,
+            'cluster_instance', OBJECT_CONSTRUCT(
+                'cluster_id', '0726-130251-aj7rmr0x',
+                'spark_context_id', '1302841573581174040'
+            ),
+            'dbt_task', OBJECT_CONSTRUCT(
+                'commands', ARRAY_CONSTRUCT(
+                    'dbt debug --target prod',
+                    'dbt deps',
+                    'dbt build --target prod --exclude databricks_monitoring dbt_dag_monitoring extraction_monitoring elementary_dbt_monitoring'
+                ),
+                'project_directory', 'dbt',
+                'schema', 'default',
+                'warehouse_id', NULL
+            ),
+            'depends_on', NULL,
+            'end_time', 172115060802344,
+            'execution_duration', 3765000,
+            'existing_cluster_id', '0726-130251-aj7rmr0x',
+            'git_source', OBJECT_CONSTRUCT(
+                'git_branch', NULL,
+                'git_provider', 'bitbucketCloud',
+                'git_snapshot', OBJECT_CONSTRUCT(
+                    'used_commit', '527373ab887ba768d1949e110fc19cac9f763fe9'
+                ),
+                'git_tag', 'production',
+                'git_url', 'https://bitbucket.org/indiciumtech/caixa_dbt_core'
+            ),
+            'libraries', ARRAY_CONSTRUCT(
+                OBJECT_CONSTRUCT('pypi', OBJECT_CONSTRUCT('package', 'dbt-databricks==1.7')),
+                OBJECT_CONSTRUCT('pypi', OBJECT_CONSTRUCT('package', 'databricks-sdk==0.28.0')),
+                OBJECT_CONSTRUCT('pypi', OBJECT_CONSTRUCT('package', 'dbt-core==1.7.10'))
+            ),
+            'notebook_task', NULL,
+            'run_id', 26793638330567,
+            'setup_duration', 284000,
+            'start_time', 172374563302374,
+            'state', OBJECT_CONSTRUCT(
+                'life_cycle_state', 'TERMINATED',
+                'result_state', 'SUCCESS',
+                'state_message', '',
+                'user_cancelled_or_timedout', FALSE
+            ),
+            'task_key', 'execute_dbt_build'
+        ),
+        OBJECT_CONSTRUCT(
+            'attempt_number', 0,
+            'cleanup_duration', 0,
+            'cluster_instance', OBJECT_CONSTRUCT(
+                'cluster_id', '0726-130251-aj7rmr0x',
+                'spark_context_id', '1302841573581174040'
+            ),
+            'dbt_task', OBJECT_CONSTRUCT(
+                'commands', ARRAY_CONSTRUCT(
+                    'dbt debug --target prod',
+                    'dbt deps',
+                    'dbt build --target prod --select elementary_dbt_monitoring'
+                ),
+                'project_directory', 'dbt',
+                'schema', 'default',
+                'warehouse_id', NULL
+            ),
+            'depends_on', ARRAY_CONSTRUCT(
+                OBJECT_CONSTRUCT('task_key', 'execute_dbt_build')
+            ),
+            'end_time', 172374563302374,
+            'execution_duration', 247000,
+            'existing_cluster_id', '0726-130251-aj7rmr0x',
+            'git_source', OBJECT_CONSTRUCT(
+                'git_branch', NULL,
+                'git_provider', 'bitbucketCloud',
+                'git_snapshot', OBJECT_CONSTRUCT(
+                    'used_commit', '527373ab887ba768d1949e110fc19cac9f763fe9'
+                ),
+                'git_tag', 'production',
+                'git_url', 'https://bitbucket.org/indiciumtech/caixa_dbt_core'
+            ),
+            'libraries', ARRAY_CONSTRUCT(
+                OBJECT_CONSTRUCT('pypi', OBJECT_CONSTRUCT('package', 'dbt-databricks==1.7')),
+                OBJECT_CONSTRUCT('pypi', OBJECT_CONSTRUCT('package', 'databricks-sdk==0.28.0')),
+                OBJECT_CONSTRUCT('pypi', OBJECT_CONSTRUCT('package', 'dbt-core==1.7.10'))
+            ),
+            'notebook_task', NULL,
+            'run_id', 276320404580646,
+            'setup_duration', 2000,
+            'start_time', 172374563302374,
+            'state', OBJECT_CONSTRUCT(
+                'life_cycle_state', 'TERMINATED',
+                'result_state', 'SUCCESS',
+                'state_message', '',
+                'user_cancelled_or_timedout', FALSE
+            ),
+            'task_key', 'execute_elementary_dbt_monitoring'
+        )
+    ),
+    'ONE_TIME',
+    '2024-08-11T07:12:47.784Z'
+);
+
 {% endset %}
 
 {% do run_query(create_table) %}
---
+++ integration_tests/macros/triggers.sql
@@ -0,0 +1,301 @@
+{% macro adf_triggers() -%}
+    {{ return(adapter.dispatch('adf_triggers')()) }}
+{%- endmacro %}
+
+{%- macro databricks__adf_triggers() -%}
+{% set create_table %}
+create or replace table `{{ target.database }}`.{{ target.schema }}.adf_triggers (
+    id STRING,
+    name STRING,
+    type STRING,
+    properties STRUCT<
+        annotations ARRAY<STRING>,
+        pipelines ARRAY<
+            STRUCT<
+                pipelineReference STRUCT<
+                    referenceName STRING,
+                    type STRING
+                >,
+                parameters STRUCT<
+                    days_before STRING,
+                    environment STRING,
+                    reset_type STRING,
+                    Job_ID STRING,
+                    DatabricksWorkspaceID STRING,
+                    WaitRecheckSeconds INT
+                >
+            >
+        >,
+        type STRING,
+        typeProperties STRUCT<
+            recurrence STRUCT<
+                frequency STRING,
+                interval INT,
+                startTime STRING,
+                timeZone STRING,
+                schedule STRUCT<
+                    minutes ARRAY<INT>,
+                    hours ARRAY<INT>,
+                    weekDays ARRAY<STRING>,
+                    monthDays ARRAY<INT>
+                >
+            >,
+            parentTrigger STRING,
+            requestedStartTime STRING,
+            requestedEndTime STRING,
+            rerunConcurrency INT
+        >,
+        runtimeState STRING
+    >
+);
+{% endset %}
+
+{% set insert_table %}
+insert into `{{ target.database }}`.{{ target.schema }}.adf_triggers VALUES 
+(
+    '/subscriptions/9f07555crvices-dev-rg/providers/Microsoft.DataFactory/factories/tlo-datastudio-adf-d/triggers/TR-fd-prod-duration_estimation-monthly',
+    'TR-fd-prod-duration_estimation-monthly',
+    'Microsoft.DataFactory/factories/triggers',
+    NAMED_STRUCT(
+        'annotations', ARRAY('fraud-detection', 'prod', 'duration-estimation'),
+        'pipelines', ARRAY(
+            NAMED_STRUCT(
+                'pipelineReference', NAMED_STRUCT(
+                    'referenceName', 'fd-prod-duration_estimation',
+                    'type', 'PipelineReference'
+                ),
+                'parameters', NAMED_STRUCT(
+                    'days_before', NULL,
+                    'environment', NULL,
+                    'reset_type', NULL,
+                    'Job_ID', NULL,
+                    'DatabricksWorkspaceID', NULL,
+                    'WaitRecheckSeconds', NULL
+                )
+            )
+        ),
+        'type', 'ScheduleTrigger',
+        'typeProperties', NAMED_STRUCT(
+            'recurrence', NAMED_STRUCT(
+                'frequency', 'Month',
+                'interval', 1,
+                'startTime', '2020-10-14T04:30:00',
+                'timeZone', 'E. South America Standard Time',
+                'schedule', NAMED_STRUCT(
+                    'minutes', ARRAY(30),
+                    'hours', ARRAY(4),
+                    'weekDays', NULL,
+                    'monthDays', ARRAY(14)
+                )
+            ),
+            'parentTrigger', NULL,
+            'requestedStartTime', NULL,
+            'requestedEndTime', NULL,
+            'rerunConcurrency', NULL
+        ),
+        'runtimeState', NULL
+    )
+),
+(
+    '/subscriptions/9f07555crvices-dev-rg/providers/Microsoft.DataFactory/factories/tlo-datastudio-adf-d/triggers/TR-fd-dev-predict-main',
+    'TR-fd-dev-predict-main',
+    'Microsoft.DataFactory/factories/triggers',
+    NAMED_STRUCT(
+        'annotations', ARRAY('fraud-detection', 'dev', 'predict'),
+        'pipelines', ARRAY(
+            NAMED_STRUCT(
+                'pipelineReference', NAMED_STRUCT(
+                    'referenceName', 'fd-dev-predict-main',
+                    'type', 'PipelineReference'
+                ),
+                'parameters', NAMED_STRUCT(
+                    'days_before', NULL,
+                    'environment', NULL,
+                    'reset_type', NULL,
+                    'Job_ID', NULL,
+                    'DatabricksWorkspaceID', NULL,
+                    'WaitRecheckSeconds', NULL
+                )
+            )
+        ),
+        'type', 'ScheduleTrigger',
+        'typeProperties', NAMED_STRUCT(
+            'recurrence', NAMED_STRUCT(
+                'frequency', 'Week',
+                'interval', 1,
+                'startTime', '2021-01-26T21:50:00',
+                'timeZone', 'E. South America Standard Time',
+                'schedule', NAMED_STRUCT(
+                    'minutes', ARRAY(0),
+                    'hours', ARRAY(5),
+                    'weekDays', ARRAY('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+                    'monthDays', NULL
+                )
+            ),
+            'parentTrigger', NULL,
+            'requestedStartTime', NULL,
+            'requestedEndTime', NULL,
+            'rerunConcurrency', NULL
+        ),
+        'runtimeState', 'Stopped'
+    )
+);
+{% endset %}
+
+{% do run_query(create_table) %}
+{% do log("finished creating table triggers", info=true) %}
+
+{% do run_query(insert_table) %}
+{% do log("finished insert table triggers", info=true) %}
+{%- endmacro -%}
+
+{%- macro snowflake__adf_triggers() -%}
+{% set create_table %}
+create or replace table `{{ target.database }}`.{{ target.schema }}.adf_triggers (
+    id VARCHAR,
+    name VARCHAR,
+    type VARCHAR,
+    properties OBJECT
+        AS (
+            annotations ARRAY,
+            pipelines ARRAY
+                AS (
+                    pipelineReference OBJECT
+                        AS (
+                            referenceName VARCHAR,
+                            type VARCHAR
+                        ),
+                    parameters OBJECT
+                        AS (
+                            days_before VARCHAR,
+                            environment VARCHAR,
+                            reset_type VARCHAR,
+                            Job_ID VARCHAR,
+                            DatabricksWorkspaceID VARCHAR,
+                            WaitRecheckSeconds INT
+                        )
+                ),
+            type VARCHAR,
+            typeProperties OBJECT
+                AS (
+                    recurrence OBJECT
+                        AS (
+                            frequency VARCHAR,
+                            interval INT,
+                            startTime VARCHAR,
+                            timeZone VARCHAR,
+                            schedule OBJECT
+                                AS (
+                                    minutes ARRAY,
+                                    hours ARRAY,
+                                    weekDays ARRAY,
+                                    monthDays ARRAY
+                                )
+                        ),
+                    parentTrigger VARCHAR,
+                    requestedStartTime VARCHAR,
+                    requestedEndTime VARCHAR,
+                    rerunConcurrency INT
+                ),
+            runtimeState VARCHAR
+        )
+);
+{% endset %}
+
+{% set insert_table %}
+INSERT INTO "{{ target.database }}.{{ target.schema }}.adf_triggers" SELECT
+    '/subscriptions/9f07555crvices-dev-rg/providers/Microsoft.DataFactory/factories/tlo-datastudio-adf-d/triggers/TR-fd-prod-duration_estimation-monthly',
+    'TR-fd-prod-duration_estimation-monthly',
+    'Microsoft.DataFactory/factories/triggers',
+    OBJECT_CONSTRUCT(
+        'annotations', ARRAY_CONSTRUCT('fraud-detection', 'prod', 'duration-estimation'),
+        'pipelines', ARRAY_CONSTRUCT(
+            OBJECT_CONSTRUCT(
+                'pipelineReference', OBJECT_CONSTRUCT(
+                    'referenceName', 'fd-prod-duration_estimation',
+                    'type', 'PipelineReference'
+                ),
+                'parameters', OBJECT_CONSTRUCT(
+                    'days_before', NULL,
+                    'environment', NULL,
+                    'reset_type', NULL,
+                    'Job_ID', NULL,
+                    'DatabricksWorkspaceID', NULL,
+                    'WaitRecheckSeconds', NULL
+                )
+            )
+        ),
+        'type', 'ScheduleTrigger',
+        'typeProperties', OBJECT_CONSTRUCT(
+            'recurrence', OBJECT_CONSTRUCT(
+                'frequency', 'Month',
+                'interval', 1,
+                'startTime', '2020-10-14T04:30:00',
+                'timeZone', 'E. South America Standard Time',
+                'schedule', OBJECT_CONSTRUCT(
+                    'minutes', ARRAY_CONSTRUCT(30),
+                    'hours', ARRAY_CONSTRUCT(4),
+                    'weekDays', NULL,
+                    'monthDays', ARRAY_CONSTRUCT(14)
+                )
+            ),
+            'parentTrigger', NULL,
+            'requestedStartTime', NULL,
+            'requestedEndTime', NULL,
+            'rerunConcurrency', NULL
+        ),
+        'runtimeState', NULL
+    )
+UNION ALL 
+SELECT
+    '/subscriptions/9f07555crvices-dev-rg/providers/Microsoft.DataFactory/factories/tlo-datastudio-adf-d/triggers/TR-fd-dev-predict-main',
+    'TR-fd-dev-predict-main',
+    'Microsoft.DataFactory/factories/triggers',
+    OBJECT_CONSTRUCT(
+        'annotations', ARRAY_CONSTRUCT('fraud-detection', 'dev', 'predict'),
+        'pipelines', ARRAY_CONSTRUCT(
+            OBJECT_CONSTRUCT(
+                'pipelineReference', OBJECT_CONSTRUCT(
+                    'referenceName', 'fd-dev-predict-main',
+                    'type', 'PipelineReference'
+                ),
+                'parameters', OBJECT_CONSTRUCT(
+                    'days_before', NULL,
+                    'environment', NULL,
+                    'reset_type', NULL,
+                    'Job_ID', NULL,
+                    'DatabricksWorkspaceID', NULL,
+                    'WaitRecheckSeconds', NULL
+                )
+            )
+        ),
+        'type', 'ScheduleTrigger',
+        'typeProperties', OBJECT_CONSTRUCT(
+            'recurrence', OBJECT_CONSTRUCT(
+                'frequency', 'Week',
+                'interval', 1,
+                'startTime', '2021-01-26T21:50:00',
+                'timeZone', 'E. South America Standard Time',
+                'schedule', OBJECT_CONSTRUCT(
+                    'minutes', ARRAY_CONSTRUCT(0),
+                    'hours', ARRAY_CONSTRUCT(5),
+                    'weekDays', ARRAY_CONSTRUCT('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+                    'monthDays', NULL
+                )
+            ),
+            'parentTrigger', NULL,
+            'requestedStartTime', NULL,
+            'requestedEndTime', NULL,
+            'rerunConcurrency', NULL
+        ),
+        'runtimeState', 'Stopped'
+    )
+;
+{% endset %}
+
+{% do run_query(create_table) %}
+{% do log("finished creating table triggers", info=true) %}
+
+{% do run_query(insert_table) %}
+{% do log("finished insert table triggers", info=true) %}
+{%- endmacro -%}
