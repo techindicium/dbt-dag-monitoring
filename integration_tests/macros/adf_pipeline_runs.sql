cd5bac05a9cb38afb2fe685bf31496871c554cf5
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-09-02 14:50:57 -0300

    bigquery in IT

--- integration_tests/macros/adf_pipeline_runs.sql
+++ integration_tests/macros/adf_pipeline_runs.sql
@@ -1,4 +1,8 @@
-{%- macro adf_pipeline_runs() -%}
+{% macro adf_pipeline_runs() -%}
+    {{ return(adapter.dispatch('adf_pipeline_runs')()) }}
+{%- endmacro %}
+
+{%- macro databricks__adf_pipeline_runs() -%}
 {% set create_table %}
 create or replace table `{{ target.database }}`.{{ target.schema }}.adf_pipeline_runs(
     id STRING,
@@ -23,52 +27,51 @@ create or replace table `{{ target.database }}`.{{ target.schema }}.adf_pipeline
     durationInMs BIGINT,
     status STRING,
     message STRING,
-    pipelineReturnValue MAP<STRING, STRING>,  -- Assuming an empty object can be represented as a map
+    pipelineReturnValue MAP<STRING, STRING>,
     lastUpdated TIMESTAMP,
     annotations ARRAY<STRING>,
-    runDimension MAP<STRING, STRING>,  -- Assuming an empty object can be represented as a map
+    runDimension MAP<STRING, STRING>,
     isLatest BOOLEAN
 );
 {% endset %}
-
 
 {% set insert_table %}
 INSERT INTO `{{ target.database }}`.{{ target.schema }}.adf_pipeline_runs
 VALUES 
 (
-    '/SUBSCRIPTITLO-DATASTUDIO-ADF-D/pipe082b73db5',
-    '9f81a5eb--833e-db6082b73db5',
-    NULL,  -- debugRunId
-    '9f81a5eb-833e-db6082b73db5',
-    'PL-FEMFILingZone-N',
+    '/SUBSCRIPTIONS/9F075ORIES/TLO-DATASTUDIO-ADF-D/pipe082b73db5',
+    '9f81a5eb-db6082b73db5',
+    NULL,
+    '9f81a5eb-a7c82b73db5',
+    'PL-FEMFILESCSLandingZone-N',
     NAMED_STRUCT(
         'ENVIRONMENT', 'FILESCSV',
         'RESET_TYPE', 'FULL',
         'DAYS_BEFORE', NULL
     ),
     NAMED_STRUCT(
-        'id', 'cf613b7b-e0c4-431d-b272-e924dc0cfbe8',
-        'name', 'cf613b7b-e0c4-431d-b272-e924dc0cfbe8',
+        'id', 'cf613b7b-e0c4cfbe8',
+        'name', 'cf613b7b-e0ce924dc0cfbe8',
         'invokedByType', 'PipelineActivity',
-        'pipelineName', 'PL-OrquestradorSourceTOBronzeZeroLoadingControl',
-        'pipelineRunId', '598ec8ee-604c-47c7-a3c0-e049c4b8c558'
+        'pipelineName', 'PL-OrquestraZeroLoadingControl',
+        'pipelineRunId', '598ec8ee-6e049c4b8c558'
     ),
     '2024-08-20T03:30:06.6061079Z',
     '2024-08-20T03:47:03.9865228Z',
     1017380,
     'Succeeded',
     NULL,
-    MAP(),  -- Empty object as a map
+    MAP(),
     '2024-08-20T03:47:03.9879388Z',
-    ARRAY(),  -- Empty array
-    MAP(),  -- Empty object as a map
+    ARRAY(),
+    MAP(),
     true
 ),
 (
-    '/SUBSCRIPTIOTASTUDIO-ADF-D/pipelinerua1-9313-73fa5c0a3f0e',
-    '64c7a8c7-30e2-4fa1-9313-73fa5c0a3f0e',
-    NULL,  -- debugRunId
-    '64c7a8c7-30e2-4fa1-9313-73fa5c0a3f0e',
+    '/SUBSCRIPTIONS/9//pipelinerua1-9313-73fa5c0a3f0e',
+    '64c7a8c7--73fa5c0a3f0e',
+    NULL,
+    '64c7a8c7-30e23fa5c0a3f0e',
     'NET_REC_DAILY_PRICE_UPDATE',
     NAMED_STRUCT(
         'ENVIRONMENT', NULL,
@@ -76,7 +79,7 @@ VALUES
         'DAYS_BEFORE', '1'
     ),
     NAMED_STRUCT(
-        'id', '08584774516819036014561066769CU22',
+        'id', '0858477451681969CU22',
         'name', 'NET_REC_DAILY_UPDATE',
         'invokedByType', 'ScheduleTrigger',
         'pipelineName', NULL,
@@ -87,14 +90,121 @@ VALUES
     4521381,
     'Succeeded',
     NULL,
-    MAP(),  -- Empty object as a map
+    MAP(),
     '2024-08-20T13:15:52.6550273Z',
     ARRAY(
         'ted'
     ),
-    MAP(),  -- Empty object as a map
+    MAP(),  
     true
 );
+{% endset %}
+
+{% do run_query(create_table) %}
+{% do log("finished creating table adf_pipeline_runs", info=true) %}
+
+{% do run_query(insert_table) %}
+{% do log("finished insert table adf_pipeline_runs", info=true) %}
+{%- endmacro -%}
+
+
+{%- macro bigquery__adf_pipeline_runs() -%}
+{% set create_table %}
+create or replace table `{{ target.database }}`.{{ target.schema }}.adf_pipeline_runs(
+    id STRING,
+    runId STRING,
+    debugRunId STRING,
+    runGroupId STRING,
+    pipelineName STRING,
+    parameters STRUCT<
+        ENVIRONMENT STRING,
+        RESET_TYPE STRING,
+        DAYS_BEFORE STRING
+    >,
+    invokedBy STRUCT<
+        id STRING,
+        name STRING,
+        invokedByType STRING,
+        pipelineName STRING,
+        pipelineRunId STRING
+    >,
+    runStart TIMESTAMP,
+    runEnd TIMESTAMP,
+    durationInMs BIGINT,
+    status STRING,
+    message STRING,
+    pipelineReturnValue ARRAY<STRUCT<key STRING, value STRING>>,
+    lastUpdated TIMESTAMP,
+    annotations ARRAY<STRING>,
+    runDimension ARRAY<STRUCT<key STRING, value STRING>>,
+    isLatest BOOLEAN
+);
+
+{% endset %}
+
+{% set insert_table %}
+INSERT INTO `{{ target.database }}.{{ target.schema }}.adf_pipeline_runs`
+VALUES 
+(
+    '/SUBSCRIPTIONS/9FFACTORIES/TLO-DATASTUDIO-ADF-D/pipe082b73db5',
+    '9f81a5eb-db6082b73db5',
+    NULL,
+    '9f81a5eb-a73e-db6082b73db5',
+    'PL-FEMFILESCSingZone-N',
+    STRUCT(
+        'FILESCSV' AS ENVIRONMENT,
+        'FULL' AS RESET_TYPE,
+        NULL AS DAYS_BEFORE
+    ),
+    STRUCT(
+        'cf613b7b-e04dc0cfbe8' AS id,
+        'cf613b7b-e0c4924dc0cfbe8' AS name,
+        'PipelineActivity' AS invokedByType,
+        'PL-OrquestradorSooLoadingControl' AS pipelineName,
+        '598ec8ee-604c-47c7-a3c0-e049c4b8c558' AS pipelineRunId
+    ),
+    TIMESTAMP('2024-08-20T03:30:06.606107Z'),
+    TIMESTAMP('2024-08-20T03:47:03.986522Z'),
+    1017380,
+    'Succeeded',
+    NULL,
+    ARRAY<STRUCT<key STRING, value STRING>>[],
+    TIMESTAMP('2024-08-20T03:47:03.987938Z'),
+    ARRAY<STRING>[],
+    ARRAY<STRUCT<key STRING, value STRING>>[],
+    TRUE
+),
+(
+    '/SUBSCRIPTIONS/9/PROVIDERS/MO-ADF-D/pipelinerua1-9313-73fa5c0a3f0e',
+    '64c7a8c7-30313-73fa5c0a3f0e',
+    NULL,
+    '64c7a8c7-30313-73fa5c0a3f0e',
+    'NET_REC_DAILY_PRICE_UPDATE',
+    STRUCT(
+        NULL AS ENVIRONMENT,
+        NULL AS RESET_TYPE,
+        '1' AS DAYS_BEFORE
+    ),
+    STRUCT(
+        '08584774516819036014561066769CU22' AS id,
+        'NET_REC_DAILY_UPDATE' AS name,
+        'ScheduleTrigger' AS invokedByType,
+        NULL AS pipelineName,
+        NULL AS pipelineRunId
+    ),
+    TIMESTAMP('2024-08-20T12:00:31.272826Z'),
+    TIMESTAMP('2024-08-20T13:15:52.654549Z'),
+    4521381,
+    'Succeeded',
+    NULL,
+    ARRAY<STRUCT<key STRING, value STRING>>[],
+    TIMESTAMP('2024-08-20T13:15:52.655027Z'),
+    ARRAY<STRING>['ted'],
+    ARRAY<STRUCT<key STRING, value STRING>>[],
+    TRUE
+);
+
+
 {% endset %}
 
 {% do run_query(create_table) %}
--- integration_tests/macros/jobs.sql
+++ integration_tests/macros/jobs.sql
@@ -1,4 +1,8 @@
-{% macro jobs() %}
+{% macro jobs() -%}
+    {{ return(adapter.dispatch('jobs')()) }}
+{%- endmacro %}
+
+{% macro databricks__jobs() %}
 {% set create_table %}
 create or replace table `{{ target.database }}`.{{ target.schema }}.jobs (
     created_time BIGINT,
@@ -38,16 +42,16 @@ create or replace table `{{ target.database }}`.{{ target.schema }}.jobs (
 INSERT INTO `{{ target.database }}`.{{ target.schema }}.jobs VALUES
 (
     CAST(1722606667504 AS BIGINT),
-    CAST('13bc3f4b-09-5444571518ae' AS STRING),
+    CAST('13bc3f4b-d299-4b58-9d09-5444571518ae' AS STRING),
     CAST(466340877826952 AS BIGINT),
     NAMED_STRUCT(
         'email_notifications', NAMED_STRUCT(
-            'on_failure',ARRAY('produtos_horizpsgenie.net'),
+            'on_failure',ARRAY('produtos_horizontais@indicium1.opsgenie.net'),
             'no_alert_for_skipped_runs', NULL
         ),
         'format', 'MULTI_TASK',
         'max_concurrent_runs', 1,
-        'name', '[prod] core_dag_mion_dbt_job',
+        'name', '[prod] core_dag_monitoring_data_transformation_dbt_job',
         'schedule', NAMED_STRUCT(
             'pause_status', NULL,
             'quartz_cron_expression', NULL,
@@ -69,16 +73,16 @@ INSERT INTO `{{ target.database }}`.{{ target.schema }}.jobs VALUES
 ),
 (
     CAST(1722544845800 AS BIGINT),
-    CAST('13bc3f4b-4571518ae' AS STRING),
+    CAST('13bc3f4b-d299-4b58-9d09-5444571518ae' AS STRING),
     CAST(823250232903490 AS BIGINT),
     NAMED_STRUCT(
         'email_notifications',NAMED_STRUCT(
-            'on_failure', ARRAY('produto1.opsgenie.net'),
+            'on_failure', ARRAY('produtos_horizontais@indicium1.opsgenie.net'),
             'no_alert_for_skipped_runs', NULL
         ),
         'format','MULTI_TASK',
         'max_concurrent_runs',1,
-        'name','[prod] core_dag_mon_meltano_job',
+        'name','[prod] core_dag_monitoring_extraction_meltano_job',
         'schedule',NAMED_STRUCT(
             'pause_status','UNPAUSED',
             'quartz_cron_expression','0 0 0/3 * * ? *',
@@ -100,11 +104,11 @@ INSERT INTO `{{ target.database }}`.{{ target.schema }}.jobs VALUES
 ),
 (
     CAST(1722538441265 AS BIGINT),
-    CAST('13bc3f4b-d444571518ae' AS STRING),
+    CAST('13bc3f4b-d299-4b58-9d09-5444571518ae' AS STRING),
     CAST(790689006770532 AS BIGINT),
     NAMED_STRUCT(
         'email_notifications',NAMED_STRUCT(
-            'on_failure',ARRAY('produtos_horizenie.net'),
+            'on_failure',ARRAY('produtos_horizontais@indicium1.opsgenie.net'),
             'no_alert_for_skipped_runs',NULL
         ),
         'format','MULTI_TASK',
@@ -126,6 +130,149 @@ INSERT INTO `{{ target.database }}`.{{ target.schema }}.jobs VALUES
             ),
             'paused_status',NULL
         )
+    ),
+    CAST('2024-08-20T09:11:36.525Z' AS TIMESTAMP)
+);
+
+{% endset %}
+
+{% do run_query(create_table) %}
+{% do log("finished creating table jobs", info=true) %}
+
+{% do run_query(insert_table) %}
+{% do log("finished insert table jobs ", info=true) %}
+
+
+{% endmacro %}
+
+{% macro bigquery__jobs() %}
+{% set create_table %}
+create or replace table `{{ target.database }}`.{{ target.schema }}.jobs (
+    created_time BIGINT,
+    creator_user_name STRING,
+    job_id BIGINT,
+    settings STRUCT<
+            email_notifications STRUCT<
+            on_failure ARRAY<STRING>,
+            no_alert_for_skipped_runs BOOLEAN
+            >,
+        format STRING,
+        max_concurrent_runs BIGINT,
+        name STRING,
+        schedule STRUCT<
+            pause_status STRING,
+            quartz_cron_expression STRING,
+            timezone_id STRING
+        >,
+        tags STRUCT<
+            dev STRING,
+            env STRING
+        >,
+        timeout_seconds bigint,
+        trigger STRUCT<
+            file_arrival STRUCT<
+                url STRING
+            >,
+            pause_status STRING
+        >
+    >,
+    insertedDate TIMESTAMP
+);
+{% endset %}
+
+{% set insert_table %}
+
+INSERT INTO `{{ target.database }}`.{{ target.schema }}.jobs VALUES
+(
+    CAST(1722606667504 AS INT64),
+    CAST('13bc3f4b-d299-4b58-9d09-5444571518ae' AS STRING),
+    CAST(466340877826952 AS INT64),
+    STRUCT(
+        STRUCT(
+            ARRAY['produtos_horizontais@indicium1.opsgenie.net'] AS on_failure,
+            NULL AS no_alert_for_skipped_runs
+        ) AS email_notifications,
+        'MULTI_TASK' AS format,
+        1 AS max_concurrent_runs,
+        '[prod] core_dag_monitoring_data_transformation_dbt_job' AS name,
+        STRUCT(
+            NULL AS pause_status,
+            NULL AS quartz_cron_expression,
+            NULL AS timezone_id
+        ) AS schedule,
+        STRUCT(
+            NULL AS dev,
+            'prod' AS env
+        ) AS tags,
+        0 AS timeout_seconds,
+        STRUCT(
+            STRUCT(
+                NULL AS url
+            ) AS file_arrival,
+            NULL AS paused_status
+        ) AS trigger
+    ),
+    CAST('2024-08-20T09:11:36.525Z' AS TIMESTAMP)
+),
+(
+    CAST(1722544845800 AS INT64),
+    CAST('13bc3f4b-d299-4b58-9d09-5444571518ae' AS STRING),
+    CAST(823250232903490 AS INT64),
+    STRUCT(
+        STRUCT(
+            ARRAY['produtos_horizontais@indicium1.opsgenie.net'] AS on_failure,
+            NULL AS no_alert_for_skipped_runs
+        ) AS email_notifications,
+        'MULTI_TASK' AS format,
+        1 AS max_concurrent_runs,
+        '[prod] core_dag_monitoring_extraction_meltano_job' AS name,
+        STRUCT(
+            'UNPAUSED' AS pause_status,
+            '0 0 0/3 * * ? *' AS quartz_cron_expression,
+            'UTC' AS timezone_id
+        ) AS schedule,
+        STRUCT(
+            NULL AS dev,
+            'prod' AS env
+        ) AS tags,
+        0 AS timeout_seconds,
+        STRUCT(
+            STRUCT(
+                NULL AS url
+            ) AS file_arrival,
+            NULL AS paused_status
+        ) AS trigger
+    ),
+    CAST('2024-08-20T09:11:36.525Z' AS TIMESTAMP)
+),
+(
+    CAST(1722538441265 AS INT64),
+    CAST('13bc3f4b-d299-4b58-9d09-5444571518ae' AS STRING),
+    CAST(790689006770532 AS INT64),
+    STRUCT(
+        STRUCT(
+            ARRAY['produtos_horizontais@indicium1.opsgenie.net'] AS on_failure,
+            NULL AS no_alert_for_skipped_runs
+        ) AS email_notifications,
+        'MULTI_TASK' AS format,
+        1 AS max_concurrent_runs,
+        '[prod] investment_postgres_extraction_spark_job' AS name,
+        STRUCT(
+            'UNPAUSED' AS pause_status,
+            '0 0 0/4 * * ? *' AS quartz_cron_expression,
+            'UTC' AS timezone_id
+        ) AS schedule,
+        STRUCT(
+            NULL AS dev,
+            'prod' AS env
+        ) AS tags,
+        0 AS timeout_seconds,
+        STRUCT(
+            STRUCT(
+                NULL AS url
+            ) AS file_arrival,
+            NULL AS paused_status
+        ) AS trigger
     ),
     CAST('2024-08-20T09:11:36.525Z' AS TIMESTAMP)
 );
