66aaa16301b114840a2533e653d34b25a16ca175
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-09-02 15:38:26 -0300

    without comment

--- integration_tests/macros/drop_schema.sql
+++ integration_tests/macros/drop_schema.sql
@@ -1,7 +1,5 @@
 {% macro drop_schema(schema_name) %}
 
-    {#- This macro will drop a single schema -#}
-
     {% set drop_schema_query %}
         DROP SCHEMA IF EXISTS {{ schema_name }} CASCADE;
     {% endset %}
