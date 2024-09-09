40a3568169df1e5cfc1e1d5fd1f4429823a6c9b9
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-09-09 15:46:32 -0300

    change path inside sh

--- integration_tests/for_CI/change_dbt_project_databricks_source.sh
+++ integration_tests/for_CI/change_dbt_project_databricks_source.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Enable databricks sources in dbt_project.yml
-sed -i '/raw_databricks_workflow_monitoring:/,/enabled:/s/enabled: false/enabled: true/' ../dbt_project.yml
+sed -i '/raw_databricks_workflow_monitoring:/,/enabled:/s/enabled: false/enabled: true/' ./dbt_project.yml
 
 # Disable airflow sources in dbt_project.yml
-sed -i 's/\(raw_airflow_monitoring:\s*\n\s*+enabled:\s*\)true/\1false/' ../dbt_project.yml
+sed -i 's/\(raw_airflow_monitoring:\s*\n\s*+enabled:\s*\)true/\1false/' ./dbt_project.yml
 
 # Disable airflow_workflow models in dbt_project.yml
-sed -i '/airflow_sources:/,/enabled:/s/enabled: true/enabled: false/' ../dbt_project.yml
+sed -i '/airflow_sources:/,/enabled:/s/enabled: true/enabled: false/' ./dbt_project.yml
 
 # Enable databricks_workflow models in dbt_project.yml
-sed -i '/databricks_workflow_sources:/,/enabled:/s/enabled: false/enabled: true/' ../dbt_project.yml
+sed -i '/databricks_workflow_sources:/,/enabled:/s/enabled: false/enabled: true/' ./dbt_project.yml
 
 # Update the enabled_sources in dbt_project.yml
-sed -i "s/enabled_sources: \[.*\]/enabled_sources: \['databricks_workflow'\]/" ../dbt_project.yml
+sed -i "s/enabled_sources: \[.*\]/enabled_sources: \['databricks_workflow'\]/" ./dbt_project.yml
