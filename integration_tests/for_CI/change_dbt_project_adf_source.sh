05f32dfaa5a45967cd3b1ee56557706713ad2ecb
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-09-09 15:39:11 -0300

    new path inside sh files

--- integration_tests/for_CI/change_dbt_project_adf_source.sh
+++ integration_tests/for_CI/change_dbt_project_adf_source.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Enable adf sources in dbt_project.yml
-sed -i '/raw_adf_monitoring:/,/enabled:/s/enabled: false/enabled: true/' dbt_project.yml
+sed -i '/raw_adf_monitoring:/,/enabled:/s/enabled: false/enabled: true/' ../dbt_project.yml
 
 # Disable databricks sources in dbt_project.yml
-sed -i 's/\(raw_databricks_workflow_monitoring:\s*\n\s*+enabled:\s*\)true/\1false/' dbt_project.yml
+sed -i 's/\(raw_databricks_workflow_monitoring:\s*\n\s*+enabled:\s*\)true/\1false/' ../dbt_project.yml
 
 # Enable adf models in dbt_project.yml
-sed -i '/adf_sources:/,/enabled:/s/enabled: false/enabled: true/' dbt_project.yml
+sed -i '/adf_sources:/,/enabled:/s/enabled: false/enabled: true/' ../dbt_project.yml
 
 # Disable databricks_workflow models in dbt_project.yml
-sed -i '/databricks_workflow_sources:/,/enabled:/s/enabled: true/enabled: false/' dbt_project.yml
+sed -i '/databricks_workflow_sources:/,/enabled:/s/enabled: true/enabled: false/' ../dbt_project.yml
 
 # Update the enabled_sources in dbt_project.yml
-sed -i "s/enabled_sources: \[.*\]/enabled_sources: \['adf'\]/" dbt_project.yml
+sed -i "s/enabled_sources: \[.*\]/enabled_sources: \['adf'\]/" ../dbt_project.yml
--- integration_tests/for_CI/change_dbt_project_airflow_source.sh
+++ integration_tests/for_CI/change_dbt_project_airflow_source.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Enable adf sources in dbt_project.yml
-sed -i '/raw_adf_monitoring:/,/enabled:/s/enabled: true/enabled: false/' dbt_project.yml
+sed -i '/raw_adf_monitoring:/,/enabled:/s/enabled: true/enabled: false/' ../dbt_project.yml
 
 # Disable databricks sources in dbt_project.yml
-sed -i 's/\(raw_airflow_monitoring:\s*\n\s*+enabled:\s*\)false/\1true/' dbt_project.yml
+sed -i 's/\(raw_airflow_monitoring:\s*\n\s*+enabled:\s*\)false/\1true/' ../dbt_project.yml
 
 # Enable adf models in dbt_project.yml
-sed -i '/adf_sources:/,/enabled:/s/enabled: true/enabled: false/' dbt_project.yml
+sed -i '/adf_sources:/,/enabled:/s/enabled: true/enabled: false/' ../dbt_project.yml
 
 # Disable databricks_workflow models in dbt_project.yml
-sed -i '/airflow_sources:/,/enabled:/s/enabled: false/enabled: true/' dbt_project.yml
+sed -i '/airflow_sources:/,/enabled:/s/enabled: false/enabled: true/' ../dbt_project.yml
 
 # Update the enabled_sources in dbt_project.yml
-sed -i "s/enabled_sources: \[.*\]/enabled_sources: \['airflow'\]/" dbt_project.yml
+sed -i "s/enabled_sources: \[.*\]/enabled_sources: \['airflow'\]/" ../dbt_project.yml
