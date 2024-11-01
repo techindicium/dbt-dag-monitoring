f737ed39038f9190d15bec0f43ec05330c5e4876
LorenzoSfredo <lorenzo.sfredo@indicium.com>
2024-11-01 16:59:19 -0300

    Fixing change_of_schema file.

--- integration_tests/for_CI/change_of_schema.sh
+++ integration_tests/for_CI/change_of_schema.sh
@@ -2,4 +2,4 @@
 
 source=$1
 
-sed -i "s/dag_monitoring_${source}_database: $2/dag_monitoring_${source}_schema: $3/" "$GITHUB_WORKSPACE/integration_tests/dbt_project.yml"
+sed -i "s/dag_monitoring_${source}_schema: $2/dag_monitoring_${source}_schema: $3/" "$GITHUB_WORKSPACE/integration_tests/dbt_project.yml"
